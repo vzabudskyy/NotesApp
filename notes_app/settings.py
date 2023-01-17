f7f3d3fa91dac3ef8a99dcb8acf87f62ff5981b5
Vladislav Zabudskyy <vladzabudskyy@knu.ua>
2023-01-17 15:20:29 +0200

    NotesApp fixed

--- notes_app/settings.py
+++ notes_app/settings.py
@@ -133,8 +133,8 @@ LOGOUT_REDIRECT_URL = "/accounts/login/"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "vladzabudskyy@knu.ua"
-EMAIL_HOST_PASSWORD = "ncllhchrvelgociz"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
 
 REST_FRAMEWORK = {
